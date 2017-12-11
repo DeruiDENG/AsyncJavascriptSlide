f5ea9ed1c1818821cb7ade4c15587fa197c987cb
Derui <moonface.ddr@gmail.com>
2017-12-11 21:33:11 +0800

    inprogress work

---
+++ js/initReveal.js
@@ -0,0 +1,6 @@
+import revealConfig from 'js/revealConfig';
+
+// More info about config & dependencies:
+// - https://github.com/hakimel/reveal.js#configuration
+// - https://github.com/hakimel/reveal.js#dependencies
+Reveal.initialize(revealConfig);
